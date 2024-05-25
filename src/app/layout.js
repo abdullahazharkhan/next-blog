0516ea3b451c97273b651a2dadb320618cd51b4f
Abdullah Azhar Khan <abdullahazharkhan1@gmail.com>
2024-05-25 10:21:27 +0500

    initial commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
 import './globals.css'
+import Navbar from "@/components/Navbar/Navbar";
+import Footer from "@/components/Footer/Footer";
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -11,7 +13,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+      <body className={inter.className}>
+        <div className='container'>
+          <div className='wrapper'>
+            <Navbar />
+            {children}
+            <Footer />
+          </div>
+        </div>
+      </body>
+    </html >
   )
 }
---
+++ src/components/CardList/CardList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./cardlist.module.css";
+import Pagination from "../Pagination/Pagination";
+
+const CardList = () => {
+  return (
+    <div className={styles.container}>
+      <Pagination />
+      CardList
+    </div>
+  );
+};
+
+export default CardList;
