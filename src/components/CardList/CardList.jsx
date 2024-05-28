74264f3e568f278d10a76a0287ece48c8e6ccc4d
Abdullah Azhar Khan <abdullahazharkhan1@gmail.com>
2024-05-28 12:34:12 +0500

    cardlist done

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import styles from "./cardlist.module.css";
 import Pagination from "../Pagination/Pagination";
+import Card from "../Card/Card";
 
 const CardList = () => {
   return (
     <div className={styles.container}>
+      <h1 className={styles.title}>Recent Posts</h1>
+      <div className={styles.posts}>
+        {/* <Card /> */}
+      </div>
       <Pagination />
       CardList
     </div>
